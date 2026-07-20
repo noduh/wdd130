a7e540f5537d36cd46c7cf3c89c461071abfe871
Noah Jorgensen <njorgensen@byui.edu>
2026-07-20 14:56:49 -0600

    added styles and scripts. renamed images

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const hamburger_button = document.getElementById("hamburger");
+const nav_menu = document.getElementById("nav-links");
+const hamburger_open_symbol = document.getElementById("open-hamburger");
+const hamburger_close_symbol = document.getElementById("close-hamburger");
+
+hamburger_button.addEventListener("click", () => toggle_navigation(nav_menu, hamburger_open_symbol, hamburger_close_symbol, hamburger_button));
+
+function toggle_navigation(nav_menu, hamburger_open_symbol, hamburger_close_symbol, hamburger_button) {
+    if (!nav_menu || !hamburger_open_symbol || !hamburger_close_symbol) {
+        return;
+    }
+
+    const is_hidden = window.getComputedStyle(nav_menu).display === "none";
+
+    nav_menu.style.display = is_hidden ? "flex" : "none";
+    hamburger_open_symbol.style.display = is_hidden ? "none" : "inline-block";
+    hamburger_close_symbol.style.display = is_hidden ? "inline-block" : "none";
+}
