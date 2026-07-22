3910868cabb8e3a96158aa7a4a787cff5602633b
Noah Jorgensen <njorgensen@byui.edu>
2026-07-22 00:24:53 -0600

    added script to remove popup when form is submitted successfully

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ const hamburger_button = document.getElementById("hamburger");
 const nav_menu = document.getElementById("nav-links");
 const hamburger_open_symbol = document.getElementById("open-hamburger");
 const hamburger_close_symbol = document.getElementById("close-hamburger");
+const cookie_form = document.getElementById("cookie-form")
+const cookie_popup = document.getElementById("cookie-popup")
 
 hamburger_button.addEventListener("click", () => toggle_navigation(nav_menu, hamburger_open_symbol, hamburger_close_symbol, hamburger_button));
 
@@ -16,3 +18,11 @@ function toggle_navigation(nav_menu, hamburger_open_symbol, hamburger_close_symb
     hamburger_open_symbol.style.display = is_hidden ? "none" : "inline-block";
     hamburger_close_symbol.style.display = is_hidden ? "inline-block" : "none";
 }
+
+cookie_form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (cookie_form.checkValidity()) {
+        cookie_popup.style.display = "none";
+    }
+});
